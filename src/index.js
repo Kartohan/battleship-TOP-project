eb2dc905343f4e1b13ecc647cd45b5214ccdb31e
Kartohan <Kartohan@gmail.com>
2022-07-24 23:39:54 +0300

    Upgrade recieveAttack/add player with methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import "./style.css";
 
 class Ship {
-  constructor(length) {
+  constructor(length, name) {
     this.length = (() => {
       let arr = [];
       for (let i = 1; i <= length; i++) {
@@ -10,6 +10,7 @@ class Ship {
       return arr;
     })();
     this.sunk = false;
+    this.name = name;
   }
   hit(position) {
     let index = this.length.indexOf(position);
@@ -38,6 +39,7 @@ class Gameboard {
       }
       return board;
     })();
+    this.size = boardSize;
     this.isGameOver = false;
   }
   putShip(x, y, ship, direction) {
@@ -60,7 +62,7 @@ class Gameboard {
         cell.ship = ship;
         cell.shipPart = ship.length[j];
         this.board[index] = cell;
-        this.makeMarked(x, i);
+        this.makeMarked(x, i, "marked");
         j++;
       }
     } else if (direction === "v") {
@@ -82,7 +84,7 @@ class Gameboard {
         cell.ship = ship;
         cell.shipPart = ship.length[j];
         this.board[index] = cell;
-        this.makeMarked(i, y);
+        this.makeMarked(i, y, "marked");
         j++;
       }
     }
@@ -98,17 +100,38 @@ class Gameboard {
       (item) => item.x === x && item.y === y
     );
     if (this.board[cellIndex].attacked) {
-      throw new Error("cannot attack in the same cell");
+      throw new Error("cannot attack in the already attacked cell");
     }
     if (this.board.find((item) => item.x === x && item.y === y).ship) {
       this.board[cellIndex].attacked = true;
       this.board[cellIndex].ship.hit(this.board[cellIndex].shipPart);
+      this.makeMarked(x, y, "attacked");
+      if (this.board.find((item) => item.x === x && item.y === y).ship.sunk) {
+        // for (
+        //   let i = 0;
+        //   i <=
+        //   this.board.find((item) => item.x === x && item.y === y).ship.length
+        //     .length;
+        //   i++
+        // ) {
+        //   let shipName = this.board[cellIndex].ship.name;
+        //   let cell = this.board.find(item => item.ship)
+        //   this.makeMarked(cell.x, cell.y, "sunk");
+        // }
+        let shipName = this.board[cellIndex].ship.name;
+        let sunkShipCells = this.board.filter(
+          (item) => item.shipPart && item.ship.name === shipName
+        );
+        for (let i = 0; i < sunkShipCells.length; i++) {
+          this.makeMarked(sunkShipCells[i].x, sunkShipCells[i].y, "sunk");
+        }
+      }
     } else {
       this.board[cellIndex].attacked = true;
     }
     this.checkGameOver();
   }
-  makeMarked(x, y) {
+  makeMarked(x, y, mark) {
     let markedCoodrsArray = [];
     for (let i = -1; i <= 1; i++) {
       for (let j = -1; j <= 1; j++) {
@@ -117,6 +140,19 @@ class Gameboard {
           y: j,
         };
         markedCoodrsArray.push(coords);
+      }
+    }
+    let markedCoordsArrayWitoutZeros = [];
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (i === 0 || j === 0) {
+          continue;
+        }
+        let coords = {
+          x: i,
+          y: j,
+        };
+        markedCoordsArrayWitoutZeros.push(coords);
       }
     }
 
@@ -129,12 +165,126 @@ class Gameboard {
       let index = this.board.findIndex(
         (item) => item.x === nextCell.x && item.y === nextCell.y
       );
-      if (this.board[index]) {
+      if (this.board[index] && mark === "marked") {
         this.board[index].marked = true;
+      } else if (this.board[index] && mark === "sunk") {
+        this.board[index].attacked = true;
+      }
+    });
+    markedCoordsArrayWitoutZeros.forEach((item) => {
+      let nextCell = {
+        x: cell.x - item.x,
+        y: cell.y - item.y,
+      };
+      let index = this.board.findIndex(
+        (item) => item.x === nextCell.x && item.y === nextCell.y
+      );
+      if (this.board[index] && mark === "attacked") {
+        this.board[index].attacked = true;
       }
     });
     return this.board;
   }
 }
 
-module.exports = { Ship, Gameboard };
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.ships = [
+      new Ship(4, "Battleship"),
+      new Ship(3, "Cruiser_one"),
+      new Ship(3, "Cruiser_two"),
+      new Ship(2, "Submarine_one"),
+      new Ship(2, "Submarine_two"),
+      new Ship(2, "Submarine_three"),
+      new Ship(1, "Patrol_one"),
+      new Ship(1, "Patrol_two"),
+      new Ship(1, "Patrol_three"),
+      new Ship(1, "Patrol_four"),
+    ];
+    this.turn = false;
+    this.gameboard = new Gameboard(10);
+  }
+  putRandomly() {
+    this.ships.forEach((ship) => {
+      let directions = ["v", "h"];
+      let randomIndex = () => {
+        return Math.round(Math.random());
+      };
+      let index = randomIndex();
+      let cells = [];
+      this.gameboard.board.forEach((item) => {
+        if (directions[index] === "v") {
+          let add = true;
+          for (let i = item.x; i < ship.length.length + item.x; i++) {
+            let checkCell = this.gameboard.board.find(
+              (cell) => cell.x === i && cell.y === item.y
+            );
+            if (checkCell == undefined || checkCell.marked) {
+              add = false;
+            }
+          }
+          if (add) {
+            cells.push(item);
+          }
+        } else if (directions[index] === "h") {
+          let add = true;
+          for (let i = item.y; i < ship.length.length + item.y; i++) {
+            let checkCell = this.gameboard.board.find(
+              (cell) => cell.x === item.x && cell.y === i
+            );
+            if (checkCell == undefined || checkCell.marked) {
+              add = false;
+            }
+          }
+          if (add) {
+            cells.push(item);
+          }
+        }
+      });
+      let randomCell = cells[Math.floor(Math.random() * cells.length)];
+      this.gameboard.putShip(
+        randomCell.x,
+        randomCell.y,
+        ship,
+        directions[index]
+      );
+    });
+  }
+  attackRandomly(enemy) {
+    let cells = enemy.gameboard.board.filter(
+      (item) => item.attacked == undefined || !item.attacked
+    );
+    let randomCell = cells[Math.floor(Math.random() * cells.length)];
+    enemy.gameboard.recieveAttack(randomCell.x, randomCell.y);
+  }
+  attackEnemy(enemy, x, y) {
+    enemy.gameboard.recieveAttack(x, y);
+  }
+}
+
+class Game {
+  constructor() {
+    this.playerOne = new Player("Player");
+    this.playerAI = new Player("Computer");
+  }
+  gameAuto() {
+    this.playerOne.putRandomly();
+    this.playerAI.putRandomly();
+    let gameOver = false;
+    let turn = 0;
+    do {
+      this.playerOne.attackRandomly(this.playerAI);
+      this.playerAI.attackRandomly(this.playerOne);
+      turn++;
+      if (
+        this.playerOne.gameboard.isGameOver === true ||
+        this.playerAI.gameboard.isGameOver === true
+      ) {
+        gameOver = true;
+      }
+    } while (gameOver === false);
+  }
+}
+
+module.exports = { Ship, Gameboard, Player };
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,8 @@
-const { Ship, Gameboard } = require("./index");
+const { Ship, Gameboard, Player } = require("./index");
 
 describe("Ship:", () => {
   test("should be instance of Ship ", () => {
-    let boat = new Ship(1);
+    let boat = new Ship(1, "Small");
     expect(boat).toBeInstanceOf(Ship);
   });
   test("should have length", () => {
@@ -12,19 +12,19 @@ describe("Ship:", () => {
     expect(boat.length.length).toBe(shipLength);
   });
   test("should have methods: hit(), isSunk()", () => {
-    let boat = new Ship(2);
+    let boat = new Ship(2, "Big");
     expect(boat).toMatchObject({
       hit: expect.any(Function),
       isSunk: expect.any(Function),
     });
   });
   test("hit() should be marked in the ship object", () => {
-    let boat = new Ship(5);
+    let boat = new Ship(5, "Big");
     boat.hit(1);
     expect(boat.length).toContain("x");
   });
   test("should sunk if all positions are hit", () => {
-    let boat = new Ship(2);
+    let boat = new Ship(2, "Big");
     boat.hit(1);
     boat.hit(2);
     expect(boat.sunk).toBe(true);
@@ -47,7 +47,7 @@ describe("Gameboard:", () => {
   });
   test("should may be able to put the ship vertically", () => {
     let playboard = new Gameboard(3);
-    let ship = new Ship(2);
+    let ship = new Ship(2, "Big");
     playboard.putShip(1, 1, ship, "v");
     expect(playboard.board).toEqual([
       { x: 1, y: 1, ship: ship, shipPart: 1, marked: true },
@@ -63,7 +63,7 @@ describe("Gameboard:", () => {
   });
   test("should may be able to put the ship horizintally", () => {
     let playboard = new Gameboard(3);
-    let ship = new Ship(2);
+    let ship = new Ship(2, "Big");
     playboard.putShip(1, 1, ship, "h");
     expect(playboard.board).toEqual([
       { x: 1, y: 1, ship: ship, shipPart: 1, marked: true },
@@ -79,8 +79,8 @@ describe("Gameboard:", () => {
   });
   test("should cannot put ship in the marked place", () => {
     let playboard = new Gameboard(3);
-    let smallShip = new Ship(1);
-    let bigShip = new Ship(2);
+    let smallShip = new Ship(1, "Small");
+    let bigShip = new Ship(2, "Big");
     playboard.putShip(1, 1, smallShip, "v");
     expect(() => playboard.putShip(2, 2, bigShip, "h")).toThrow(
       "cannot be placed near the another ship"
@@ -88,7 +88,7 @@ describe("Gameboard:", () => {
   });
   test("should cannot put ship outside the playfield", () => {
     let playboard = new Gameboard(3);
-    let bigShip = new Ship(2);
+    let bigShip = new Ship(2, "Big");
     expect(() => playboard.putShip(3, 3, bigShip, "h")).toThrow(
       "cannot be placed outside the field"
     );
@@ -104,7 +104,7 @@ describe("Gameboard:", () => {
   });
   test("should be able to recieve hit attack", () => {
     let playboard = new Gameboard(2);
-    let ship = new Ship(1);
+    let ship = new Ship(1, "Small");
     playboard.putShip(1, 1, ship, "v");
     playboard.recieveAttack(1, 1, ship);
     expect(playboard.board[0]).toEqual({
@@ -122,13 +122,39 @@ describe("Gameboard:", () => {
     let playboard = new Gameboard(2);
     playboard.recieveAttack(1, 2);
     expect(() => playboard.recieveAttack(1, 2)).toThrow(
-      "cannot attack in the same cell"
+      "cannot attack in the already attacked cell"
     );
   });
+  test("should mark 'attacked' around the ship when it sunk", () => {
+    let playboard = new Gameboard(2);
+    let ship = new Ship(1, "Small");
+    playboard.putShip(1, 1, ship, "v");
+    playboard.recieveAttack(1, 1);
+    expect(() => playboard.recieveAttack(1, 2)).toThrow(
+      "cannot attack in the already attacked cell"
+    );
+    expect(() => playboard.recieveAttack(2, 2)).toThrow(
+      "cannot attack in the already attacked cell"
+    );
+    expect(() => playboard.recieveAttack(2, 1)).toThrow(
+      "cannot attack in the already attacked cell"
+    );
+  });
+  test('should mark "attacked" in the diagonal cell', () => {
+    let playboard = new Gameboard(2);
+    let ship = new Ship(2, "Big");
+    playboard.putShip(1, 1, ship, "v");
+    playboard.recieveAttack(1, 1);
+    expect(() => playboard.recieveAttack(2, 2)).toThrow(
+      "cannot attack in the already attacked cell"
+    );
+    expect();
+  });
+
   test("should finish the game after all ships are sunk", () => {
     let playboard = new Gameboard(3);
-    let smallShip = new Ship(1);
-    let bigShip = new Ship(2);
+    let smallShip = new Ship(1, "Small");
+    let bigShip = new Ship(2, "Big");
     playboard.putShip(1, 1, smallShip, "v");
     playboard.putShip(3, 2, bigShip, "h");
     playboard.recieveAttack(1, 1);
@@ -140,5 +166,35 @@ describe("Gameboard:", () => {
     expect(smallShip.sunk).toBe(true);
     expect(bigShip.sunk).toBe(true);
     expect(playboard.isGameOver).toBe(true);
+  });
+});
+
+describe("Player:", () => {
+  test("should be instance of Player", () => {
+    expect(new Player()).toBeInstanceOf(Player);
+  });
+  test("should have a name", () => {
+    let playerOne = new Player("name");
+    expect(playerOne.name).toEqual("name");
+  });
+  test("should have a ships array", () => {
+    let playerOne = new Player("player");
+    expect(playerOne.ships).toBeInstanceOf(Array);
+  });
+  test("should have a turn", () => {
+    let playerOne = new Player("player");
+    expect(playerOne.turn).toBe(false);
+  });
+  test("should have a gameboard", () => {
+    let playerOne = new Player("player");
+    expect(playerOne.gameboard).toBeInstanceOf(Gameboard);
+  });
+  test("can attack enemy gameboard", () => {
+    let playerOne = new Player("player one");
+    let playerTwo = new Player("player two");
+    playerOne.putRandomly();
+    playerTwo.putRandomly();
+    playerOne.attackEnemy(playerTwo, 1, 1);
+    expect(playerTwo.gameboard.board[0].attacked).toBe(true);
   });
 });
